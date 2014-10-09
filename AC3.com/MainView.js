5dfd83dd6c0c63c659bc33f490e45945d4fdc74c
Chance.Williams <chancewilliams171@gmail.com>
2014-10-09 10:18:00 -0400

    Main View

---
+++ AC3.com/MainView.js
@@ -0,0 +1,11 @@
+$(function() {
+    $( "#date1" ).datepicker();
+  });
+  
+ $(function() {
+    $( "#date2" ).datepicker();
+  });
+  
+ $( "#sensor" ).selectmenu();
+ 
+ $( "#deactivated" ).selectmenu();
