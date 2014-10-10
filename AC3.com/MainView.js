c2bbe6d28fa85cdbffc5dfaffb587e203d8f94d3
Chance.Williams <chancewilliams171@gmail.com>
2014-10-10 17:39:47 -0400

    Graph and styling

--- AC3.com/MainView.js
+++ AC3.com/MainView.js
@@ -9,3 +9,25 @@ $(function() {
  $( "#sensor" ).selectmenu();
  
  $( "#deactivated" ).selectmenu();
+ 
+ $(document).ready(function(){
+var data = [
+    ['User1', 75],['User2', 15],['User3', 30], 
+    ['User4', 43],['User5', 42]
+  ];
+  var plot1 = jQuery.jqplot ('eventactors', [data], 
+    { 
+	  title: 'User Event Occurrence',
+      seriesDefaults: {
+        // Make this a pie chart.
+        renderer: jQuery.jqplot.PieRenderer, 
+        rendererOptions: {
+          // Put data labels on the pie slices.
+          // By default, labels show the percentage of the slice.
+          showDataLabels: true
+        }
+      },
+      legend: { show:true, location: 'e' }
+    }
+  );
+});
