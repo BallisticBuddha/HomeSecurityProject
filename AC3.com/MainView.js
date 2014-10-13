e8176f9e90dbfe44f41f711c4806c6f5dacd3c97
chance.williams <chance@fabric.com>
2014-10-13 12:04:35 -0400

    Sensor Chart

--- AC3.com/MainView.js
+++ AC3.com/MainView.js
@@ -30,4 +30,24 @@ var data = [
       legend: { show:true, location: 'e' }
     }
   );
+  
+  var data2 = [
+    ['Kitchen', 10],['Dinning', 36],['Bedroom', 30], 
+    ['Garage', 25],['Deck', 50]
+  ];
+  var plot2 = jQuery.jqplot ('sensors', [data2], 
+    { 
+	  title: 'Sensor Event Occurrence',
+      seriesDefaults: {
+        // Make this a pie chart.
+        renderer: jQuery.jqplot.PieRenderer, 
+        rendererOptions: {
+          // Put data labels on the pie slices.
+          // By default, labels show the percentage of the slice.
+          showDataLabels: true
+        }
+      },
+      legend: { show:true, location: 'e' }
+    }
+  );
 });
